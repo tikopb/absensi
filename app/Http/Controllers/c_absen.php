6bde9fdefcb224dd71439add6cb9d9b8f9b0ef49
tiko <tiko.pb@gmail.com>
2020-10-05 07:47:18 +0700

    change get into first()
    merubah get() menjadi first() untuk keperluan pengambilan data.

--- app/Http/Controllers/c_absen.php
+++ app/Http/Controllers/c_absen.php
@@ -23,20 +23,19 @@ class c_absen extends Controller
         $barcode = $request->input('in_app_barcode');
         $whereKaryawan = array('is_active'=>'t',
                         'barcode'=>$barcode);
-        $d_karyawan = $M_karyawan->view_data('karyawans',$whereKaryawan)->get();
+        $d_karyawan = $M_karyawan->view_data('karyawans',$whereKaryawan)->first();
 
         if(!empty($d_karyawan)){
             $whereshift = array ('is_active'=>'t',
                              'karyawan_id'=>$d_karyawan['karyawan_id'],
                              'tanggal'=>now());
-            $d_shift = $M_shift->view_data('shifts',$whereshift)->get();
+            $d_shift = $M_shift->view_data('shifts',$whereshift)->frist();
             if(!empty($d_shift)){
                 $whereshiftHour = array ('is_active'=>'t',
                                         'shifthours_id'=>$d_shift['shifthours_id']);
-                $d_shifthour = $M_shifthour->view_data('shifthours',$whereshiftHour)->get();
+                $d_shifthour = $M_shifthour->view_data('shifthours',$whereshiftHour)->first();
                 if(!empty($d_shifthour)){
-                    // get now()
-
+                    
                 }
             }
         }
