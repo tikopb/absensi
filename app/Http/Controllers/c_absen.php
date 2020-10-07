8adbb72c551b1244d4136f693f05aaed43f2a6af
tiko <tiko.pb@gmail.com>
2020-10-07 11:05:47 +0700

    make function

--- app/Http/Controllers/c_absen.php
+++ app/Http/Controllers/c_absen.php
@@ -7,6 +7,7 @@ use App\Models\M_Karyawan;
 use App\Models\M_shift;
 use App\Models\M_absen;
 use App\Models\M_shifthour;
+use App\Models\M_absen;
 use Carbon\Carbon;
 
 class c_absen extends Controller
@@ -18,15 +19,17 @@ class c_absen extends Controller
     //app
     public function process_absen(Request $request)
     {
-        $now = Carbon::now()->format('H:m');
+        $now = Carbon::now()->format('H:i');
+        //dd($now);
         $M_karyawan = new M_karyawan;
         $M_shift = new M_shift;
         $M_shifthour = new M_shifthour;
+        $M_Absen = new M_absen;
         $barcode = $request->input('in_app_barcode');
         $whereKaryawan = array('is_active'=>'t',
                         'barcode'=>$barcode);
         $d_karyawan = $M_karyawan->view_data('karyawans',$whereKaryawan)->first();
-        //dd($d_karyawan);
+      
         if(!empty($d_karyawan)){
             $whereshift = array ('is_active'=>'t',
                              'karyawan_id'=>$d_karyawan['karyawan_id'],
@@ -37,8 +40,13 @@ class c_absen extends Controller
                                         'shifthours_id'=>$d_shift['shifthours_id']);
                 $d_shifthour = $M_shifthour->view_data('shifthours',$whereshiftHour)->first();
                 if(!empty($d_shifthour)){
-                    if($now > $d_shifthour['in']){
-
+                    // cari apakah pada tabel absen sudah tertinput untuk hari ini jika blm maka 
+                    // dianggap sebagai in jika sudah ada maka dianggap sebagai out
+                    $in = $d_shifthour['in'];
+                    if($now > $in){
+                       //input absen tepat waktu 
+                    }else if($now < $in){
+                        //input absen terlambat.
                     }
                 }
             }
