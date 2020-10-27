7ea20a4f9ff3de7603826d5979f24fa4d4f23a09
tiko <tiko.pb@gmail.com>
2020-10-27 20:58:51 +0700

    fix bug

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -47,7 +47,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="#" class="brand-link">
-      <img src="{{ asset('image/w.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="{{ asset('image/w.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: 1">
       <span class="brand-text font-weight-light">Absensi AJS System</span>
     </a>
 
